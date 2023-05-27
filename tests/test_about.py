74e256401638a11eea1621bd99cbe3057be650b8
rebecca <tyvek_zhang@126.com>
2023-05-27 14:48:44 +0800

    Add tox -part1 format code function is ok

--- tests/test_about.py
+++ tests/test_about.py
@@ -12,6 +12,6 @@ def test_version():
     path = os.path.join(parent_path, "src/py_learning", "__about__.py")
     print(parent_path)
     print(path)
-    with open(os.path.join(parent_path, "src/py_learning", "__about__.py"), "r", encoding="utf-8") as f:
+    with open(os.path.join(parent_path, "src/py_learning", "__about__.py"), encoding="utf-8") as f:
         exec(f.read(), about)
     assert "__version__" in about
